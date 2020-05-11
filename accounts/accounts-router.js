524659fcb937a8fbdf055e378c0a6f7b26bbdf91
robertelias <robert.elias@live.com>
2020-05-11 12:59:43 -0700

    Get api/accounts

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -4,5 +4,23 @@ const db = require('../data/dbConfig.js');
 
 const router = express.Router();
 
+//localhost:4000/api/accounts
+// should initial be empty array no data
+router.get("/", (req, res) => {
+    //list of accounts
+    //SELECT * FROM [accounts]
+    //All database operations return a promise
+    db.select("*")
+      .from("accounts")
+      .then(accounts => {
+        res.status(200).json(accounts);
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({ error: "failed to get data from accounts" });
+      });
+  });
+
+
 
 module.exports = router;
